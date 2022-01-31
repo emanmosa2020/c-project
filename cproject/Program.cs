9644f70963dc4c8e573f14bab002669a646e5fa7
eman ashraf <eman.ashraf.mosa@gmail.com>
2022-01-31 12:32:29 +0200

    first commit

---
+++ cproject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+
+            double myres= Sub.subfunction(5, 2);
+            Console.WriteLine(myres);
+            
+        }
+    }
+}
---
+++ cproject/Sub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cproject
+{
+    internal class Sub
+    {
+        public static double subfunction(double x,double y)
+        {
+            double res;
+            res = x - y;
+            return res;
+        }
+    }
+}
